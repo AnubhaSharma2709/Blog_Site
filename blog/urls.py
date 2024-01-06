f6dfa3ddc193b8ac1d1e8e4770c201b521ac7207
AnubhaSharma2709 <anubhaa.sharma27@gmail.com>
2024-01-07 00:01:10 +0530

    working on views.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
+from .import views 
+
 urlpatterns = [
-    path(""),
-    path("posts"),
-    path("posts/<slug>") # /posts/my-first-post --> concept called Slug
+    path("", views.starting_page),
+    path("posts", views.posts),
+    path("posts/<slug>", views.post_details) # /posts/my-first-post --> concept called Slug
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render
+
+def starting_page(request):
+    return None 
+
+def post(request):
+    return None 
+
+def post_details(request):
+    return None 
